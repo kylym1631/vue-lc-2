7a4031b0c2a8458b773328b52c352e237a61c84a
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2023-01-23 14:44:59 +0100

    19 From Mixins to Composables

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,11 @@
+<script setup>
+import {useFlash} from "../composables/useFlash";
+
+let {flash} = useFlash();
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div >
+    <button @click="flash('Yay','It works on about page','info')">Click me</button>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,11 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+import {useFlash} from "../composables/useFlash";
 
-// eslint-disable-next-line no-unused-vars
-let message = $ref("halo");
-
-let doSomething = () =>{
-  alert('doing sth');
-}
-setTimeout(()=>{
-  message = 'I have been changed';
-},2000)
-
+let {flash} = useFlash();
 </script>
 
 <template>
   <main>
-    <TheWelcome />
-    <p>{{message}}</p>
-    <p><input type="text" v-model="message"></p>
-    <button @click="doSomething">Click me</button>
+    <button @click="flash('Test','It works')">Click me</button>
   </main>
 </template>
