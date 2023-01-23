bf9175c4f2391eb51981f204a781d692e52a457f
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2023-01-23 14:17:26 +0100

    18Two Mental Leaps to Script Setup :

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup>
 import TheWelcome from "../components/TheWelcome.vue";
+
+// eslint-disable-next-line no-unused-vars
+let message = $ref("halo");
+
+let doSomething = () =>{
+  alert('doing sth');
+}
+setTimeout(()=>{
+  message = 'I have been changed';
+},2000)
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{message}}</p>
+    <p><input type="text" v-model="message"></p>
+    <button @click="doSomething">Click me</button>
   </main>
 </template>
