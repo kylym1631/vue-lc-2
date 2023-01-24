6e415c40d66fc73ebc895c5b20d44086f324a1b2
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2023-01-25 00:44:15 +0100

    26 Code Organization

--- src/router/index.js
+++ src/router/index.js
@@ -34,6 +34,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/CounterView.vue"),
     },
+    {
+      path: "/team",
+      name: "team",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/TeamView.vue"),
+    },
   ],
 });
 
