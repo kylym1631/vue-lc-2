26ced3bb66455f0383eb0efb378e304173910b22
kylym1631 <67018582+kylym1631@users.noreply.github.com>
2023-01-27 00:57:08 +0100

    28Build a Modal Component

--- src/components/Teams/TeamHeader.vue
+++ src/components/Teams/TeamHeader.vue
@@ -7,7 +7,9 @@ let team = useTeamStore();
   <header class="flex justify-between">
     <div>
       <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
-              :disabled="! team.spotsRemaining">Add Member ({{ team.spotsRemaining }} Spots Left)</button>
+              :disabled="! team.spotsRemaining"
+              @click="$emit('add')"
+      >Add Member ({{ team.spotsRemaining }} Spots Left)</button>
     </div>
 
     <div>
